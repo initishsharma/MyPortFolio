a83a0f653fc189112aa40e6ea1d9a7c5044625b0
Nitish Sharma <nitish.sharma@cybergindia.local>
2021-04-14 17:40:36 +0530

    JS Enabled Footer button

---
+++ script/script.js
@@ -0,0 +1,33 @@
+function myfunction()
+{
+   window.open("https://www.facebook.com/sharma.nitish72",'_blank');
+}
+
+function myfunction1()
+{
+   window.open("https://www.twitter.com/initishsharmaa",'_blank');
+}
+
+function myfunction2()
+{
+   var email = "sharma.nitish72@gmail.com"
+      var mailto_link = 'mailto:' + email
+      window = window.open(mailto_link, 'emailWindow')
+      if (window && window.open && !window.closed)         
+          window.close()
+}
+
+function myfunction3()
+{
+   window.open("https://www.linkedin.com/in/nitish-sharma-226420132/",'_blank');
+}
+
+function myfunction4()
+{
+   var tel = "+91-7456031322"
+      var tel_link = 'tel:' + tel
+      window = window.open(tel_link, 'emailWindow')
+      if (window && window.open && !window.closed)         
+          window.close()
+}
+
